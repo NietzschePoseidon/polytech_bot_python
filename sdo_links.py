bbc3a24e37e11c08138dd719fc46f3155de6874f
Ellis <147318799+NietzschePoseidon@users.noreply.github.com>
2026-09-05 17:05:04 +0300

    Create sdo_links.py

---
+++ sdo_links.py
@@ -0,0 +1,13 @@
+"""
+Список ссылок на СДО (lms.spbstu.ru) для предметов.
+"""
+
+SDO_LINKS = {
+    "ВышМат": "https://lms.spbstu.ru/course/view.php?id=20239",
+    "Физика": "https://lms.spbstu.ru/course/view.php?id=20244",
+    "АнглЯз": "-",
+    "ЭкоЭво": "https://dl-ibmst.spbstu.ru/course/view.php?id=6180",
+    "ВышМат доп": "https://dl-ibmst.spbstu.ru/course/view.php?id=6127",
+    "ТехнЛидерство": "https://dl-ibmst.spbstu.ru/course/view.php?id=6163"
+    # Добавляй сюда остальные предметы
+}
